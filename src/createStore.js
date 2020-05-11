324be1cbba666dd71483b39c34ddc5a4981f3c1d
Mikhail Tsyvis <mikhail_tsyvis@epam.com>
2020-05-11 23:30:49 +0300

    simple redux done

---
+++ src/createStore.js
@@ -0,0 +1,20 @@
+export function createStore(rootReducer) {
+    let state = {};
+    const subscribers = [];
+
+    // we should use замыкания because js don't have privite methods. There are public methods after return
+    // все что храниться в функции createStore будут приватные переменные
+    return {
+        // action === { type: 'INCREMENT' }
+        dispatch(action) {
+            state = rootReducer(state, action);
+            subscribers.forEach(sub => sub());
+        },
+        subscribe(callback) {
+            subscribers.push(callback);
+        },
+        getState() {
+            return state;
+        }
+    }
+}
---
+++ src/native.js
@@ -0,0 +1,23 @@
+import './styles.css'
+import {createStore} from './createStore'
+
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+const asyncBtn = document.getElementById('async');
+const themeBtn = document.getElementById('theme');
+
+addBtn.addEventListener('click', () => {
+
+})
+
+subBtn.addEventListener('click', () => {
+    
+})
+
+asyncBtn.addEventListener('click', () => {
+    
+})
+
+themeBtn.addEventListener('click', () => {
+    
+})
